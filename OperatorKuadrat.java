19f07e6776c66cc5e6822aa26cadf10a0a9a57ec
Siti Huraerah <sitihuraerah@gmail.com>
2016-03-29 20:58:01 +0700

    Added files via upload

---
+++ OperatorKuadrat.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operatorkuadrat;
+
+/**
+ *
+ * @author SITI
+ */
+public class OperatorKuadrat {
+
+static int OperatorKuadrat(int bil){
+     return bil*bil;
+ }
+    public static void main(String[] args) {
+       int n=15;
+       System.out.println(n +" Kuadrat dari 15 adalah "+ OperatorKuadrat(n));
+    }
+    
+}
